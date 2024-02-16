125fed0dee413c147716ecd117aff45a36126b5a
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-16 12:52:23 +0000

    영어 명언 구글 번역 작업중

--- chrome/quotes.js
+++ chrome/quotes.js
@@ -62,15 +62,60 @@ fetch('https://api.quotable.io/quotes?tags=inspirational')//여러개
 .catch(error => console.error('API 호출 중 오류 발생:', error));
 */
 fetch('https://api.quotable.io/random')//랜덤 한개
-.then(response => response.json())
-.then(data => {
-  if (data.length > 0) {
-    console.log('랜덤 Inspirational 명언:', data.content);
-    console.log('저자:', data.author);
-    quote.innerText = `"${data.content}"`;
-    author.innerText = `-${data.author}`;
-  } else {
-    console.log('명언이 없습니다.');
-  }
-})
-.catch(error => console.error('API 호출 중 오류 발생:', error));
+  .then(response => response.json())
+  .then(data => {
+    if (data.length > 0) {
+      console.log('랜덤 Inspirational 명언:', data.content);
+      console.log('저자:', data.author);
+      quote.innerText = `"${data.content}"`;
+      author.innerText = `-${data.author}`;
+      translateToKorean(data.content);//번역
+    } else {
+      console.log('명언이 없습니다.');
+    }
+  })
+  .catch(error => console.error('API 호출 중 오류 발생:', error));
+
+//////////////////////////////////////////////////////////////
+async function translateToKorean(englishQuote) {
+  const response = await fetch('https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ko&dt=t&q=' + encodeURIComponent(englishQuote));
+  const data = await response.json();
+  console.log(data)
+  //return data[0][0][0];
+  let krTrans = '';
+  data[0].forEach(e=>{console.log(e[0]);
+                     krTrans+=e[0]});
+  console.log(krTrans);
+  //return krTrans;
+  let trans = document.createElement('p');
+  trans.innerText = krTrans;
+  console.log(trans);
+  document.getElementById("quote").appendChild(trans);
+}
+/*
+
+// Quotable API로부터 영어 명언 가져오기
+async function getEnglishQuote() {
+  const response = await fetch('https://api.quotable.io/random');
+  const data = await response.json();
+  return data.content;
+}
+
+// 명언 가져와서 한국어로 번역하기
+async function getKoreanQuote() {
+  const englishQuote = await getEnglishQuote();
+  const koreanQuote = await translateToKorean(englishQuote);
+  return koreanQuote;
+}
+
+// 명언 가져와서 출력하기
+async function displayQuotes() {
+  const englishQuote = await getEnglishQuote();
+  const koreanQuote = await getKoreanQuote();
+  console.log('English Quote:', englishQuote);
+  console.log('Korean Quote:', koreanQuote);
+}
+
+// 호출
+displayQuotes();
+ */
