9cd6bb842f7d248dea209487c2fc511da99f4a2c
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-16 12:10:51 +0000

    랜덤 명언 가져오게

--- chrome/quotes.js
+++ chrome/quotes.js
@@ -1,4 +1,4 @@
-const quotes = [  {
+/*const quotes = [  {
   quote: "The way to get started is to quit talking and begin doing.",
   author: "Walt Disney",
 },
@@ -39,11 +39,38 @@ const quotes = [  {
   quote: "Travel expands the mind and fills the gap.",
   author: "Sheda Savage",
 },
-];
+];*/
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
-const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+//const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
-quote.innerText = todaysQuote.quote;
-author.innerText = todaysQuote.author;
+//quote.innerText = todaysQuote.quote;
+//author.innerText = todaysQuote.author;
+
+/*
+fetch('https://api.quotable.io/quotes?tags=inspirational')//여러개
+.then(response => response.json())
+.then(data => {
+  if (data.results.length > 0) {
+    console.log('랜덤 Inspirational 명언:', data.results[0].content);
+    console.log('저자:', data.results[0].author);
+  } else {
+    console.log('해당 태그의 명언이 없습니다.');
+  }
+})
+.catch(error => console.error('API 호출 중 오류 발생:', error));
+*/
+fetch('https://api.quotable.io/random')//랜덤 한개
+.then(response => response.json())
+.then(data => {
+  if (data.length > 0) {
+    console.log('랜덤 Inspirational 명언:', data.content);
+    console.log('저자:', data.author);
+    quote.innerText = `"${data.content}"`;
+    author.innerText = `-${data.author}`;
+  } else {
+    console.log('명언이 없습니다.');
+  }
+})
+.catch(error => console.error('API 호출 중 오류 발생:', error));
