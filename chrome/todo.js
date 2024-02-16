0bfca504d490c3a12141d99e8141d64f06cb6c72
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-16 11:46:37 +0000

    todo 체크박스 추가 체크 토글 줄긋기

--- chrome/todo.js
+++ chrome/todo.js
@@ -23,11 +23,14 @@ function deletetodo(event) {
 function paintToDo(newTodo) {
   const li = document.createElement('li');
   li.id = newTodo.id;
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
   const span = document.createElement('span');
   span.innerText = newTodo.text;
   const button = document.createElement('button');
   button.innerText = '❌';
   button.addEventListener('click', deletetodo);
+  li.appendChild(checkbox);
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
@@ -60,3 +63,30 @@ if (savedToDos !== null) {
 }
 //JSON.parse(localStorage.getItem(TODOS_KEY));
 
+/*
+*맨위에 한개만
+  document.querySelector('#todo-list input[type="checkbox"]').addEventListener('change', toggleLineThrough);
+
+function toggleLineThrough(checkbox) {
+    var listItem = checkbox.target.parentNode;
+    listItem.classList.toggle('checked');
+}
+*/
+/* 동적 생성 된 애 안됨
+  let checkboxes = document.querySelectorAll('#todo-list input[type="checkbox"]');
+
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+        var listItem = this.parentNode;
+        listItem.classList.toggle('checked');
+    });
+});
+*/
+// 부모 요소에 이벤트 리스너 추가
+  toDoList.addEventListener('change', function(event) {
+    // 이벤트가 발생한 요소가 체크박스인 경우에만 실행
+    if (event.target.matches('input[type="checkbox"]')) {
+        var listItem = event.target.parentNode;
+        listItem.classList.toggle('checked');
+    }
+});
