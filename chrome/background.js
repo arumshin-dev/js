77f8305ae67c7fcee2c13914734209bc23da2cfd
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-16 11:58:20 +0000

    배경 랜덤으로 사진 바뀌게

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,11 @@
-const images = ["https://raw.githubusercontent.com/nomadcoders/javascript-for-beginners/5874df713ba5476174c5df78dc886198c38ef8dc/img/0.jpeg", "https://img.freepik.com/free-vector/watercolor-oil-painting-background_52683-107025.jpg?w=1800&t=st=1707820466~exp=1707821066~hmac=d6a55406d7e23b27fe1d006254698e12371ce3c87618324e4ba04ed491404107", "https://blog.kakaocdn.net/dn/eFxmqN/btquZFlxvLG/IKittvt0eFXbhGhj2MuJOK/img.jpg"];
+/*const images = ["https://raw.githubusercontent.com/nomadcoders/javascript-for-beginners/5874df713ba5476174c5df78dc886198c38ef8dc/img/0.jpeg", "https://img.freepik.com/free-vector/watercolor-oil-painting-background_52683-107025.jpg?w=1800&t=st=1707820466~exp=1707821066~hmac=d6a55406d7e23b27fe1d006254698e12371ce3c87618324e4ba04ed491404107", "https://blog.kakaocdn.net/dn/eFxmqN/btquZFlxvLG/IKittvt0eFXbhGhj2MuJOK/img.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const chosenImage = images[Math.floor(Math.random() * images.length)];*/
 
 //const bgImage = document.createElement("img");
 const bgImage = document.querySelector(".background-image");
 
-bgImage.src = `${chosenImage}`;
+//bgImage.src = `${chosenImage}`;
+bgImage.src = 'https://picsum.photos/1200/800'; // 무작위 이미지 URL
 
 //document.body.appendChild(bgImage);
