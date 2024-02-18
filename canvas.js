ef1721cc2471b5d3c01d0a9adc7439e309ccff41
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-02-18 12:07:14 +0000

    사람모양 원

--- canvas.js
+++ canvas.js
@@ -37,3 +37,27 @@ ctx.lineTo(575,100);
 ctx.lineTo(400,200);
 //ctx.stroke();
 ctx.fill();
+
+//사람
+ctx.beginPath();
+ctx.fillStyle= "#FBCEB1";
+ctx.fillRect(100,500,20,100);//왼팔
+ctx.fillRect(200,500,20,100);//오른
+ctx.fillRect(130,500,60,150);//몸통
+
+ctx.arc(160,440,50,0,2*Math.PI);//얼굴 원
+ctx.fill();//원채워
+
+ctx.beginPath();
+ctx.fillStyle= 'black';
+ctx.arc(140,430,7,Math.PI,2*Math.PI);//왼쪽 눈
+ctx.arc(180,430,7,Math.PI,2*Math.PI);//오른쪽 눈;
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle="red";
+ctx.arc(160,470,10,0,1*Math.PI);//;
+ctx.fill();
+
+ctx.beginPath();
+ctx.tri
